d183fd62d9dc613d8e562aaab554326f65f12d6a
Mickaël Anderson <123479860+RaMiAnderson@users.noreply.github.com>
2024-10-10 21:43:27 +0200

    road to use axios

--- server/server.js
+++ server/server.js
@@ -5,6 +5,18 @@ const app = express();
 
 // Middleware pour parser les données POST
 app.use(express.urlencoded({ extended: true }));
+// Middleware pour parser les JSON
+app.use(express.json());
+// Middleware pour Cors
+const cors = require("cors");
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    Credential: true
+}));
+
+
+
 
 //DATABASE
 const {initDataBase} = require("./database/databaseConnector");
