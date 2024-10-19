ec25887c818558bfb5a1c4f8bd8af349f2e74514
Mickaël Anderson <123479860+RaMiAnderson@users.noreply.github.com>
2024-10-19 21:02:44 +0200

    NavBar finished

--- client/src/components/Navbar/AdminNavbar.jsx
+++ client/src/components/Navbar/AdminNavbar.jsx
@@ -1,18 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+// Icons
+import DashBoardIco from "@mui/icons-material/InsertChartOutlinedOutlined"
+import ArticleIco from "@mui/icons-material/DescriptionOutlined"
+import FournisseurIco from "@mui/icons-material/AssignmentIndOutlined"
+import ClientIco from "@mui/icons-material/ContactPageOutlined"
+import InventoryIcon from "@mui/icons-material/Inventory2Outlined"
+import ApprovIco from "@mui/icons-material/ArchiveOutlined"
+import DepenseIco from "@mui/icons-material/PriceChangeOutlined"
+import UserIco from "@mui/icons-material/BadgeOutlined"
+import DecoIco from "@mui/icons-material/LogoutOutlined"
+import Toggle from "@mui/icons-material/CompareArrowsOutlined"
 
 import bonHomm from "../../assets/img/Principale/userPers.png"
+
 
 //Style
 import "./navBarStyle.css"
 
 export default function AdminNavbar() {
+  const [indexActivList, setIndexActivList] = useState(1);
+  const [isToggleActive, setInverseToggle] = useState(false );
+
+  
+
+  let ifClick = (index) => {
+    setIndexActivList(index);
+
+  }
+
+  const activeToggle = () => {
+    setInverseToggle(!isToggleActive)
+  }
+  
   return (
     <>
-        <div className='parentNavbar'>
+        <div className = {`parentNavbar ${isToggleActive ? "Active-Toggle" : ""}`} >
           {/* LOGO */}
           <div className="logoSection">
-            <div className='logoFile'>Icon</div>
+            <div className='logoFile'>sary</div>
             <p>Mylane H</p>
+          </div>
+
+          <div className="toggleSection">
+            <button onClick={() => activeToggle()} className='btnToggle'><Toggle sx={{width:35, height:35}}/></button>
           </div>
 
           {/* InfoUser */}
@@ -29,66 +60,61 @@ export default function AdminNavbar() {
           {/* Link section  */}
           <div className="linkSection">
             <ul>
-              <a href="#">
-                <li className='selected'>
-                  <div className="iconList"></div>
-                  <div className="nameList">Dashboard</div>
+              
+                <li className={`${indexActivList === 1 ? "selected" : ""}`} onClick={() => ifClick(1)} >
+                  <div className="iconList"><DashBoardIco sx={{width: 27, height:27}} /></div>
+                  <div className="nameList">Statistique</div>
                 </li>
-              </a>
-              <a href="#">
-                <li>
-                  <div className="iconList"></div>
+      
+                <li className={`${indexActivList === 2 ? "selected" : ""}`} onClick={() => ifClick(2)}>
+                  <div className="iconList"><ArticleIco sx={{width: 27, height:27}}/></div>
                   <div className="nameList">Articles</div>
                 </li>
-              </a>
-              <a href="#">
-                <li>
-                  <div className="iconList"></div>
+         
+        
+                <li className={`${indexActivList === 3 ? "selected" : ""}`} onClick={() => ifClick(3)}>
+                  <div className="iconList"><FournisseurIco sx={{width: 27, height:27}}/></div>
                   <div className="nameList">Fournisseurs</div>
                 </li>
-              </a>
-              <a href="#">
-                <li>
-                  <div className="iconList"></div>
+           
+                <li className={`${indexActivList === 4 ? "selected" : ""}`} onClick={() => ifClick(4)}>
+                  <div className="iconList"><ClientIco sx={{width: 27, height:27}}/></div>
                   <div className="nameList">Clients</div>
                 </li>
-              </a>
-              <a href="#">
-                <li>
+            
+                <li className={`${indexActivList === 5 ? "selected" : ""}`} onClick={() => ifClick(5)}>
                   {/* liste des stock, date dernier reaprovisionemt, qt */}
-                  <div className="iconList"></div>
+                  <div className="iconList"><InventoryIcon sx={{width: 27, height:27}}/></div>
                   <div className="nameList">Iventaires</div>
                 </li>
-              </a>
-              <a href="#">
-                <li>
+         
+                <li className={`${indexActivList === 6 ? "selected" : ""}`} onClick={() => ifClick(6)}>
                   {/* mampiditr qt article */}
-                  <div className="iconList"></div>
-                  <div className="nameList">Approvisionmt</div>
+                  <div className="iconList"><ApprovIco sx={{width: 27, height:27}}/></div>
+                  <div className="nameList">Ravitaillement</div>
                 </li>
-              </a>
-              <a href="#">
-                <li>
-                  <div className="iconList"></div>
+           
+                <li className={`${indexActivList === 7 ? "selected" : ""}`} onClick={() => ifClick(7)}>
+                  <div className="iconList"><DepenseIco sx={{width: 27, height:27}}/></div>
                   <div className="nameList">Dépenses</div>
                 </li>
-              </a>
-              <a href="#">
-                <li>
-                  <div className="iconList"></div>
+            
+                <li className={`${indexActivList === 8 ? "selected" : ""}`} onClick={() => ifClick(8)}>
+                  <div className="iconList"><UserIco sx={{width: 27, height:27}}/></div>
                   <div className="nameList">Utilisateurs</div>
                 </li>
-              </a>
+          
             </ul>
 
           </div>
 
           {/* section deco */}
-          <a href="#">
-            <div className='logoutSection'>
-              Déconnexion
+ 
+            <div className={`logoutSection ${indexActivList === 9 ? "selected" : ""}`} onClick={() => ifClick(9)}>
+              <div ><DecoIco sx={{width: 27, height:27}}/></div>
+              <div className='nameList'>Déconnexion</div>
             </div>
-          </a>
+
         </div>
     </>
   )
